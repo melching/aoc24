5ec56de7ce303d1075a51840e75679a4206d709d
Christian Melching <20678978+melching@users.noreply.github.com>
2024-12-23 15:23:33 +0100

    day 23 part 1 solved, the wrong way

---
+++ day_23/src/main.rs
@@ -0,0 +1,165 @@
+use std::collections::{HashMap, HashSet};
+use std::fs;
+use std::time::Instant;
+
+fn read_file() -> String {
+    let content = fs::read_to_string("./input").expect("Something went wrong reading the file");
+    return content;
+}
+
+fn parse_input(input: &String) -> HashMap<&str, HashSet<&str>> {
+    let mut computers: HashMap<&str, HashSet<&str>> = HashMap::new();
+
+    for line in input.lines() {
+        let conn: Vec<&str> = line.split("-").collect();
+        assert_eq!(conn.len(), 2);
+        computers.entry(conn[0]).or_default().insert(conn[1]);
+        computers.entry(conn[1]).or_default().insert(conn[0]);
+    }
+    computers
+}
+
+fn find_loops<'a>(
+    graph: &'a HashMap<&'a str, HashSet<&'a str>>,
+    start: &'a str,
+    current: &'a str,
+    seen: &mut HashSet<&'a str>,
+    distance: usize,
+) -> Vec<HashSet<&'a str>> {
+    seen.insert(current);
+
+    // we've gone too far
+    if seen.len() > distance {
+        return Vec::new();
+    }
+
+    // we have our loop
+    let connected = graph.get(current).unwrap();
+    if seen.len() == distance && connected.contains(start) {
+        return vec![seen.clone()];
+    }
+
+    let mut loops: Vec<HashSet<&str>> = Vec::new();
+    // lets look further
+    for comp in connected {
+        // we don't want loops in loops
+        if seen.contains(comp) {
+            continue;
+        }
+
+        // lets look further
+        // Note that this will add duplicates into the list that have to be filtered later on
+        loops.extend(find_loops(graph, start, comp, &mut seen.clone(), distance));
+    }
+
+    loops
+}
+
+fn main() {
+    let input: String = read_file();
+    let now = Instant::now();
+
+    // part one
+    let computers = parse_input(&input);
+
+    let mut all_loops: Vec<HashSet<&str>> = Vec::new();
+    for (comp, _) in computers.iter() {
+        if !comp.starts_with("t") {
+            continue;
+        }
+        let mut seen: HashSet<&str> = HashSet::new();
+        for l in find_loops(&computers, &comp, &comp, &mut seen, 3) {
+            if !all_loops.contains(&l) {
+                all_loops.push(l);
+            }
+        }
+    }
+    println!(
+        "Found {} loops that contain computer start with t",
+        all_loops.len()
+    );
+
+    // part two
+
+    let elapsed = now.elapsed();
+    println!("Elapsed: {:.2?}", elapsed);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn get_test_content() -> String {
+        "kh-tc
+qp-kh
+de-cg
+ka-co
+yn-aq
+qp-ub
+cg-tb
+vc-aq
+tb-ka
+wh-tc
+yn-cg
+kh-ub
+ta-co
+de-co
+tc-td
+tb-wq
+wh-td
+ta-ka
+td-qp
+aq-cg
+wq-ub
+ub-vc
+de-ta
+wq-aq
+wq-vc
+wh-yn
+ka-de
+kh-ta
+co-tc
+wh-qp
+tb-vc
+td-yn"
+            .to_string()
+    }
+    #[test]
+    fn test_solution_1() {
+        let input = get_test_content();
+        let computers = parse_input(&input);
+
+        let mut all_loops: Vec<HashSet<&str>> = Vec::new();
+        for (comp, _) in computers.iter() {
+            let mut seen: HashSet<&str> = HashSet::new();
+            for l in find_loops(&computers, &comp, &comp, &mut seen, 3) {
+                if !all_loops.contains(&l) {
+                    all_loops.push(l);
+                }
+            }
+        }
+
+        assert_eq!(all_loops.len(), 12)
+    }
+
+    #[test]
+    fn test_solution_1_1() {
+        let input = get_test_content();
+        let computers = parse_input(&input);
+
+        let mut all_loops: Vec<HashSet<&str>> = Vec::new();
+        for (comp, _) in computers.iter() {
+            if !comp.starts_with("t") {
+                continue;
+            }
+            let mut seen: HashSet<&str> = HashSet::new();
+            for l in find_loops(&computers, &comp, &comp, &mut seen, 3) {
+                if !all_loops.contains(&l) {
+                    all_loops.push(l);
+                }
+            }
+        }
+
+        assert_eq!(all_loops.len(), 7)
+    }
+}
